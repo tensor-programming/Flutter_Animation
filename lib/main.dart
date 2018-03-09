b7b219994368038109987a8e1e3578b41f8d6213
Tensor-Programming <abeltensor@tensor-programming.com>
2018-03-09 17:23:28 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  Animation animation;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        duration: Duration(milliseconds: 2000), vsync: this);
+
+    animation = Tween(begin: 0.0, end: 1000.0).animate(animationController);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.forward();
+      }
+    });
+    animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LogoAnimation(
+      animation: animation,
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+}
+
+class LogoAnimation extends AnimatedWidget {
+  LogoAnimation({Key key, Animation animation})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    Animation animation = listenable;
+    return FadeTransition(
+      opacity: animation, //changes Opacity of the Logo
+      child: Container(
+        height: 400.0,
+        width: 400.0,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
